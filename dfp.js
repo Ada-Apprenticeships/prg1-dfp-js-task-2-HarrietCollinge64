532b5999bb1e5a2df77c8e57bcc51918f3e8c1a2
HarrietCollinge64 <harriet.collinge@ada.ac.uk>
2024-10-23 10:48:47 +0000

    comments added and cleaned up

--- dfp.js
+++ dfp.js
@@ -2,34 +2,30 @@ const fs = require('fs');
 
 
 function parseFile (indata, outdata, delimiter = ";") {
-  if (fs.existsSync(outdata)) {
-    fs.unlinkSync(outdata);
-  }
+
+  //removed output file if already exists
+  fs.existsSync(outdata) ? fs.unlinkSync(outdata) : false
+
   try {
     const data = fs.readFileSync(indata, "utf-8")
-    let review 
-    let sentiment
     let count = 0
     const lines = data.split(/\n/)
-    delimiter = lines[0].at(6)
+
+    //use the specified delimiter from the first line if not provided
+    delimiter = delimiter || lines[0].at(6);
+
     for (let i = 1; i < lines.length; i++) {
+      lineSplit = lines[i].split(delimiter)
+      const review = lineSplit[0].trim().substring(0,20)
+      const sentiment = lineSplit[1].trim()
+      fs.appendFileSync(outdata, `${sentiment}${delimiter}${review}\n`, 'utf-8')
       count++
-      lineArr = lines[i].split(delimiter)
-      review = lineArr[0].trim().substring(0,20)
-      sentiment = lineArr[1].trim()
-      fs.appendFileSync(outdata, `${sentiment}${delimiter}${review}\n`, "utf-8")
     }
     return count
   } catch (error) {
     return -1
   }
 }
-
-
-
-  parseFile("datafile.csv", "outputdata.csv")
-
-
 
 // Leave this code here for the automated tests
 module.exports = {
